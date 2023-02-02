e2b9a9288d621189b85755b93453c062ba8c1043
fee3fitri <safitri.shelton@gmail.com>
2023-02-01 22:35:58 -0800

    Add ts jQuery type definition from npm

--- src/demo.ts
+++
@@ -1,8 +0,0 @@
-class Contact {
-  id;
-  name;
-  birthDate;
-}
-
-// To run the ts code, type "tsc" in the console
-// It will compile the code in js and create new js file (in this case "demo.js")
