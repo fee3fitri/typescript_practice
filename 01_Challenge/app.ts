a564195d1c447e1b32f00d84d0ea9b598b580766
fee3fitri <safitri.shelton@gmail.com>
2023-02-01 22:50:33 -0800

    Convert js file to ts file

---
+++ 01_Challenge/app.ts
@@ -0,0 +1,13 @@
+const formattedDate = formatDate(new Date())
+
+console.log(`Today's date is ${formattedDate}`)
+
+/*
+
+To convert js file to ts file:
+1. Install ts in the root folder
+2. Create tsconfig.json with all necessary configuration
+3. Change the file extension from .js to .ts
+4. Check if everything's fine by compile the ts using "tsc" command
+
+*/
