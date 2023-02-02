41d6fcdfbdc0ef116fbebcbdecc5eac4ab1b4849
fee3fitri <safitri.shelton@gmail.com>
2023-02-01 22:19:33 -0800

    Utilize typescript type on regular JS file

---
+++ 01_01/src/demo.ts
@@ -0,0 +1,8 @@
+class Contact {
+  id;
+  name;
+  birthDate;
+}
+
+// To run the ts code, type "tsc" in the console
+// It will compile the code in js and create new js file (in this case "demo.js")
