f20e50ba796684341d8d5a7605516458fbc23dd4
fee3fitri <safitri.shelton@gmail.com>
2023-02-06 13:04:58 -0800

    Extending and modifying existing types using utility type

--- 03_05/src/demo.ts
+++ 03_05/src/demo.ts
@@ -22,6 +22,7 @@ interface Contact {
     name: string;
     status: ContactStatus;
     address: Address;
+    email: string;
 }
 
 interface Query {
@@ -29,8 +30,34 @@ interface Query {
     matches(val): boolean;
 }
 
+// Defining ContactQuery type to be used on line 36
+// Add Partial to choose properties that I want to include and exclude in the new type
+// Partial defines all the properties on Contact and Query objects as optional
+// Add Omit to omit certain properties from the cloned definitions.
+// Omit requires 2nd parameter which is the list of property names that I'd like to omit
+// type ContactQuery = Omit<
+//     Partial<
+//         Record<keyof Contact, Query>
+//     >,
+//     "address" | "status"
+// >
+
+// Add Pick is the opposite of Omit above
+// Pick requires 2nd parameter which is the list of property names that is required
+type ContactQuery = Partial<
+    Pick<
+        Record<keyof Contact, Query>,
+        "id" | "name"
+    >
+>
+
+// Required type is the opposite of Partial
+// It will turn all the optional properties to the required ones
+// The RequiredContactQuery is inherited the same properties as ContactQuery above
+type RequiredContactQuery = Required<ContactQuery>
+
 // Using Record on query parameter to have the same property type as Contact and the same value type as Query object
-function searchContacts(contacts: Contact[], query: Record<keyof Contact, Query>) {
+function searchContacts(contacts: Contact[], query: ContactQuery) {
     return contacts.filter(contact => {
         for (const property of Object.keys(contact) as (keyof Contact)[]) {
             // get the query object for this property
